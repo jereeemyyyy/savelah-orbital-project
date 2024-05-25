0bd37fba8f575d0fca7ae4db23170cb2245bd5fa
jereeemyyyy <jeremysimwenze@gmail.com>
2024-05-25 16:33:24 +0800

    Added Home,Welcome,Login,SignUp screens into 'content' for nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/index.{js,jsx,ts,tsx}"],
+  content: ["./app/index.{js,jsx,ts,tsx}", 
+            "./screens/HomeScreen.{js,jsx,ts,tsx}",
+            "./screens/WelcomeScreen.{js,jsx,ts,tsx}",
+            "./screens/LoginScreen.{js,jsx,ts,tsx}",
+            "./screens/SignUpScreen.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
   },
